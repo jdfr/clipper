efd7c1fae9a7466ce9de7c11ccb8d7848aeba19d
josedavid <jdfr@users.noreply.github.com>
2015-11-23 12:40:11 +0100

    major refactoring to avoid hardcoded application termination if IO fails

--- iopaths/iopaths.cpp
+++ iopaths/iopaths.cpp
@@ -7,25 +7,7 @@ typedef clp::Path::iterator pi;
 typedef DPaths::iterator dsi;
 typedef DPath::iterator di;
 
-//we assume that sizeof(clp::cInt)==sizeof(double)
 
-/*if SAFEWAY is not defined, we assume that:
-     * vectors are contiguous
-     * sizeof(clp::IntPoint)==(2*sizeof(clp::cInt))
-     * IntPoint.X comes before IntPoint.Y
-*/
-//#define SAFEWAY
-
-
-
-#ifdef DEBUG_IOPATHS
-  FILE *STDERR=stderr;
-  void setIOErrOutput(FILE* err) {
-    STDERR = err;
-  }
-#endif
-
-//this assumes that sizeof(clp::cInt)==sizeof(double)==8
 int getPathsSerializedSize(clp::Paths &paths, PathCloseMode mode) {
     int s = 0;
     for (psi path = paths.begin(); path != paths.end(); ++path) {
@@ -38,259 +20,168 @@ int getPathsSerializedSize(clp::Paths &paths, PathCloseMode mode) {
     return s;
 }
 
-void   readDoublePaths(FILE *f, clp::Paths  &paths, double scalingfactor) {
+#define  READ_BINARY_INCLASS(start, size, num, f)   {size_t numio = fread  ((start), (size), (num), (f)); if (numio!=(num)) { errs.push_back(IOErr(f, false, MY_FUNCTION)); return false; } }
+#define WRITE_BINARY_INCLASS(start, size, num, f)   {size_t numio = fwrite ((start), (size), (num), (f)); if (numio!=(num)) { errs.push_back(IOErr(f, true,  MY_FUNCTION)); return false; } }
+
+
+bool IOPaths::readDoublePaths(clp::Paths  &paths, double scalingfactor) {
     int64 numpaths, numpoints;
 
-    READ_BINARY(&numpaths, sizeof(int64), 1, f);
+    READ_BINARY_INCLASS(&numpaths, sizeof(int64), 1, f);
 
     size_t oldsize = paths.size(), newsize = oldsize + numpaths;
 
     paths.resize(newsize);
     double v[2];
     for (psi path = paths.begin() + oldsize; path != paths.end(); ++path) {
-        READ_BINARY(&numpoints, sizeof(int64), 1, f);
+        READ_BINARY_INCLASS(&numpoints, sizeof(int64), 1, f);
         path->resize(numpoints);
         pi pathend = path->end();
         for (pi point = path->begin(); point != pathend; ++point) {
-            READ_BINARY(v, sizeof(double), 2, f);
+            READ_BINARY_INCLASS(v, sizeof(double), 2, f);
             point->X = (int64)(v[0] * scalingfactor);
             point->Y = (int64)(v[1] * scalingfactor);
         }
     }
+    return true;
 }
 
-void   readDoublePaths(FILE *f, DPaths  &paths) {
+bool   IOPaths::readDoublePaths(DPaths  &paths) {
     int64 numpaths, numpoints;
 
-    READ_BINARY(&numpaths, sizeof(int64), 1, f);
+    READ_BINARY_INCLASS(&numpaths, sizeof(int64), 1, f);
 
     size_t oldsize = paths.size(), newsize = oldsize + numpaths;
 
     paths.resize(newsize);
     for (dsi path = paths.begin() + oldsize; path != paths.end(); ++path) {
-        READ_BINARY(&numpoints, sizeof(double), 1, f);
+        READ_BINARY_INCLASS(&numpoints, sizeof(double), 1, f);
         path->resize(numpoints);
-        READ_BINARY(&path->front(), sizeof(double), 2*path->size(), f);
+        READ_BINARY_INCLASS(&path->front(), sizeof(double), 2 * path->size(), f);
     }
+    return true;
 }
 
-void  writeDoublePaths(FILE *f, DPaths &paths, PathCloseMode mode) {
+bool  IOPaths::writeDoublePaths(DPaths &paths, PathCloseMode mode) {
     int64 numpaths = paths.size(), numpoints, numpointsdeclared;
-    WRITE_BINARY(&numpaths, sizeof(int64), 1, f);
+    WRITE_BINARY_INCLASS(&numpaths, sizeof(int64), 1, f);
 
     bool addlast = (mode == PathLoop) && (paths.size() > 0);
     for (dsi path = paths.begin(); path != paths.end(); ++path) {
         numpoints = path->size();
         numpointsdeclared = addlast ? (numpoints + 1) : numpoints;
         int64 num = numpoints * 2;
-        WRITE_BINARY(&numpointsdeclared, sizeof(int64), 1, f);
-        WRITE_BINARY(&((*path)[0]), sizeof(double), num, f);
+        WRITE_BINARY_INCLASS(&numpointsdeclared, sizeof(int64), 1, f);
+        WRITE_BINARY_INCLASS(&((*path)[0]), sizeof(double), num, f);
         if (addlast) {
-            WRITE_BINARY(&((*path)[0]), sizeof(double), 2, f);
+            WRITE_BINARY_INCLASS(&((*path)[0]), sizeof(double), 2, f);
         }
     }
+    return true;
 }
 
-void  writeDoublePaths(FILE *f, clp::Paths  &paths, double scalingfactor, PathCloseMode mode) {
-    writeDoublePaths(FILES(1, f), paths, scalingfactor, mode);
-}
+bool  IOPaths::writeDoublePaths(clp::Paths  &paths, double scalingfactor, PathCloseMode mode) {
 
-void  writeDoublePaths(FILES fs, clp::Paths  &paths, double scalingfactor, PathCloseMode mode) {
     int64 numpaths = paths.size(), numpoints, numpointsdeclared;
-    for (FILES::iterator f = fs.begin(); f != fs.end(); ++f) {
-        WRITE_BINARY(&numpaths, sizeof(int64), 1, *f);
-    }
+    WRITE_BINARY_INCLASS(&numpaths, sizeof(int64), 1, f);
 
     bool addlast = (mode == PathLoop) && (paths.size() > 0);
     double v[2];
     for (psi path = paths.begin(); path != paths.end(); ++path) {
         numpoints = path->size();
         numpointsdeclared = addlast ? (numpoints + 1) : numpoints;
-        for (FILES::iterator f = fs.begin(); f != fs.end(); ++f) {
-            WRITE_BINARY(&numpointsdeclared, sizeof(int64), 1, *f);
-        }
+        WRITE_BINARY_INCLASS(&numpointsdeclared, sizeof(int64), 1, f);
         pi pathend = path->end();
         for (pi point = path->begin(); point != pathend; ++point) {
             v[0] = point->X * scalingfactor;
             v[1] = point->Y * scalingfactor;
-            for (FILES::iterator f = fs.begin(); f != fs.end(); ++f) {
-                WRITE_BINARY(v, sizeof(double), 2, *f);
-            }
+            WRITE_BINARY_INCLASS(v, sizeof(double), 2, f);
         }
         if (addlast) {
             v[0] = path->front().X * scalingfactor;
             v[1] = path->front().Y * scalingfactor;
-            for (FILES::iterator f = fs.begin(); f != fs.end(); ++f) {
-                WRITE_BINARY(v, sizeof(double), 2, *f);
-            }
+            WRITE_BINARY_INCLASS(v, sizeof(double), 2, f);
         }
     }
+    return true;
 }
 
 
-void readClipperPaths(FILE *f, clp::Paths &paths) {
+bool IOPaths::readClipperPaths(clp::Paths &paths) {
     int64 numpaths, numpoints;
     
-    READ_BINARY(&numpaths, sizeof(int64), 1, f);
+    READ_BINARY_INCLASS(&numpaths, sizeof(int64), 1, f);
     
     size_t oldsize = paths.size(), newsize = oldsize+numpaths;
     
     paths.resize(newsize);
     for (psi path = paths.begin()+oldsize; path!=paths.end(); ++path) {
-        READ_BINARY(&numpoints, sizeof(int64), 1, f);
+        READ_BINARY_INCLASS(&numpoints, sizeof(int64), 1, f);
         path->resize(numpoints);
-        #ifdef SAFEWAY
-            pi pathend = path->end();
-            for (pi point=path->begin(); point!=pathend; ++point) {
-                READ_BINARY(&point->X, sizeof(int64), 1, f);
-                READ_BINARY(&point->Y, sizeof(int64), 1, f);
-            }
-        #else
             size_t num = 2*numpoints;
-            READ_BINARY(&((*path)[0]), sizeof(int64), num, f);
-        #endif
+            READ_BINARY_INCLASS(&((*path)[0]), sizeof(int64), num, f);
     }
+    return true;
 }
 
-void writeClipperPaths(FILE *f, clp::Paths &paths, PathCloseMode mode) {
+bool IOPaths::writeClipperPaths(clp::Paths &paths, PathCloseMode mode) {
     int64 numpaths = paths.size(), numpoints, numpointsdeclared;
-#ifdef DEBUG_IOPATHS
-    fprintf(STDERR, "In writeClipperPaths, writing numpaths=%lld\n", numpaths); fflush(STDERR);
-#endif
-    WRITE_BINARY(&numpaths, sizeof(int64), 1, f);
-    
-#ifdef DEBUG_IOPATHS
-    size_t k=0;
-#endif
+
+    WRITE_BINARY_INCLASS(&numpaths, sizeof(int64), 1, f);
     bool addlast = (mode == PathLoop) && (paths.size() > 0);
     for (psi path = paths.begin(); path!=paths.end(); ++path) {
         numpoints = path->size();
         numpointsdeclared = addlast ? (numpoints+1) : numpoints;
-#ifdef DEBUG_IOPATHS
-        fprintf(STDERR, "In writeClipperPaths (path %llu/%llu), writing numpoints=%lld\n", k++, paths.size(), numpoints); fflush(STDERR);
-#endif
-        WRITE_BINARY(&numpointsdeclared, sizeof(int64), 1, f);
-#        ifdef SAFEWAY
-            pi pathend = path->end();
-#         ifdef DEBUG_IOPATHS
-            fprintf(STDERR, "In writeClipperPaths, writing %lld points\n", path->size());
-            size_t kk=0;
-#         endif
-            for (pi point=path->begin(); point!=pathend; ++point) {
-#         ifdef DEBUG_IOPATHS
-                fprintf(STDERR, "    point %llu: %lld, %lld\n", kk++, point->X, point->Y); fflush(STDERR);
-#         endif
-                WRITE_BINARY(&point->X, sizeof(int64), 1, f);
-                WRITE_BINARY(&point->Y, sizeof(int64), 1, f);
-            }
-#        else
-            size_t num = 2*numpoints;
-#         ifdef DEBUG_IOPATHS
-            fprintf(STDERR, "In writeClipperPaths, writing %lld longints (%lld points)\n", num, numpoints);
-            clp::IntPoint *d1 = &((*path)[0]);
-            int64 *d2 = (int64*)(&((*path)[0]));
-            for (int64 kk=0; kk<numpoints; ++kk) {
-                fprintf(STDERR, "    point %lld (style A): %lld, %lld\n", kk, d1->X, d1->Y); d1++;
-                int64 a = *(d2++);
-                int64 b = *(d2++);
-                fprintf(STDERR, "    point %lld (style B): %lld, %lld\n", kk, a, b);
-            }
-              fflush(STDERR);
-#         endif
-            WRITE_BINARY(&((*path)[0]), sizeof(int64), num, f);
-#        endif
-            if (addlast) {
-                WRITE_BINARY(&((*path)[0]), sizeof(int64), 2, f);
-            }
-    }
-
-
-}
-
-
-void  writeClipperSlice (FILE *f, clp::Paths &paths, std::vector<double> &zs, PathCloseMode mode) {
-    int64 numzs = zs.size();
-    
-#ifdef DEBUG_IOPATHS
-    fprintf(STDERR, "In writeClipperSlice, writing numzs=%lld\n", numzs); fflush(STDERR);
-#endif
-    WRITE_BINARY(&numzs, sizeof(int64), 1, f);
-
-    #ifdef SAFEWAY
-        for (di z = zs.begin(); z!=zs.end(); ++z) {
-            WRITE_BINARY(&*z, sizeof(double), 1, f);
+        WRITE_BINARY_INCLASS(&numpointsdeclared, sizeof(int64), 1, f);
+        size_t num = 2*numpoints;
+        WRITE_BINARY_INCLASS(&((*path)[0]), sizeof(int64), num, f);
+        if (addlast) {
+            WRITE_BINARY_INCLASS(&((*path)[0]), sizeof(int64), 2, f);
         }
-    #else
-    if (numzs > 0) {
-#ifdef DEBUG_IOPATHS
-        fprintf(STDERR, "In writeClipperSlice, writing %lld doubles\n", numzs); fflush(STDERR);
-#endif
-        WRITE_BINARY(&(zs[0]), sizeof(double), numzs, f);
     }
-    #endif
-    writeClipperPaths(f, paths, mode);//PathLoop);
+    return true;
 }
 
-void  writePrefixedClipperPaths(FILE *f, clp::Paths &paths, PathCloseMode mode) {
-    int64 numpaths = paths.size(), numpoints, numpointsdeclared;
-#ifdef DEBUG_IOPATHS
-    fprintf(STDERR, "In writePrefixedClipperPaths, writing numpaths=%lld\n", numpaths); fflush(STDERR);
-    int k=0;
-#endif
 
-    WRITE_BINARY(&numpaths, sizeof(int64), 1, f);
+bool  IOPaths::writePrefixedClipperPaths(clp::Paths &paths, PathCloseMode mode) {
+    int64 numpaths = paths.size(), numpoints, numpointsdeclared;
+
+    WRITE_BINARY_INCLASS(&numpaths, sizeof(int64), 1, f);
     bool addlast = (mode == PathLoop) && (paths.size() > 0);
     for (psi path = paths.begin(); path != paths.end(); ++path) {
         numpoints = path->size();
         numpointsdeclared = addlast ? (numpoints + 1) : numpoints;
-#ifdef DEBUG_IOPATHS
-    fprintf(STDERR, "In writePrefixedClipperPaths, writing numpointsdeclared=%lld for path %d\n", numpointsdeclared, k); fflush(STDERR);
-    k++;
-#endif
-        WRITE_BINARY(&numpointsdeclared, sizeof(int64), 1, f);
+        WRITE_BINARY_INCLASS(&numpointsdeclared, sizeof(int64), 1, f);
     }
 
-#ifdef DEBUG_IOPATHS
-    k=0;
-#endif
     for (psi path = paths.begin(); path != paths.end(); ++path) {
         numpoints = path->size();
         size_t num = 2 * numpoints;
-#ifdef DEBUG_IOPATHS
-    fprintf(STDERR, "In writePrefixedClipperPaths, writing path %d with num=%lld\n", k, num); fflush(STDERR);
-    k++;
-#endif
-        WRITE_BINARY(&((*path)[0]), sizeof(int64), num, f);
+        WRITE_BINARY_INCLASS(&((*path)[0]), sizeof(int64), num, f);
         if (addlast) {
-            WRITE_BINARY(&((*path)[0]), sizeof(int64), 2, f);
+            WRITE_BINARY_INCLASS(&((*path)[0]), sizeof(int64), 2, f);
         }
     }
+    return true;
 }
 
-void readPrefixedClipperPaths(FILE *f, clp::Paths &paths) {
+bool IOPaths::readPrefixedClipperPaths(clp::Paths &paths) {
     int64 numpaths, numpoints;
 
-    READ_BINARY(&numpaths, sizeof(int64), 1, f);
+    READ_BINARY_INCLASS(&numpaths, sizeof(int64), 1, f);
 
     size_t oldsize = paths.size(), newsize = oldsize + numpaths;
 
     paths.resize(newsize);
 
     for (psi path = paths.begin() + oldsize; path != paths.end(); ++path) {
-        READ_BINARY(&numpoints, sizeof(int64), 1, f);
+        READ_BINARY_INCLASS(&numpoints, sizeof(int64), 1, f);
         path->resize(numpoints);
     }
 
     for (psi path = paths.begin() + oldsize; path != paths.end(); ++path) {
-#ifdef SAFEWAY
-        pi pathend = path->end();
-        for (pi point = path->begin(); point != pathend; ++point) {
-            READ_BINARY(&point->X, sizeof(int64), 1, f);
-            READ_BINARY(&point->Y, sizeof(int64), 1, f);
-        }
-#else
         size_t num = 2 * path->size();
-        READ_BINARY(&((*path)[0]), sizeof(int64), num, f);
-#endif
+        READ_BINARY_INCLASS(&((*path)[0]), sizeof(int64), num, f);
     }
+    return true;
 }
--- iopaths/iopaths.hpp
+++ iopaths/iopaths.hpp
@@ -5,6 +5,13 @@
 
 #include "common.hpp"
 
+/*we assume that:
+* sizeof(clp::cInt)==sizeof(double)
+* vectors are contiguous
+* sizeof(clp::IntPoint)==(2*sizeof(clp::cInt))
+* IntPoint.X comes before IntPoint.Y
+*/
+
 #if defined(_MSC_VER) //(defined(_WIN32) || defined(_WIN64))
   #define MY_FILENAME __FILE__
   #define MY_FUNCTION __FUNCTION__
@@ -19,18 +26,11 @@
 
 #define IOERR(f, num, numio) {fprintf(stderr, "IOERROR IN LINE %s OF %s, function %s. ERRNO: %d. INTENDED/REAL IO BYTES: %d/%d\n", LINE_STRING, MY_FILENAME, MY_FUNCTION, ferror((f)), (num), (numio)); exit(-1);}
 
-#define  READ_BINARY(start, size, num, f)   {size_t numio = fread  ((start), (size), (num), (f)); if (numio!=(num)) IOERR((f), (num), numio); }
-#define WRITE_BINARY(start, size, num, f)   {size_t numio = fwrite ((start), (size), (num), (f)); if (numio!=(num)) IOERR((f), (num), numio); }
-
-//#define DEBUG_IOPATHS
-#ifdef DEBUG_IOPATHS
-  void setIOErrOutput(FILE* err);
-#endif
+#define  READ_BINARY_NAIVE(start, size, num, f)   {size_t numio = fread  ((start), (size), (num), (f)); if (numio!=(num)) IOERR((f), (num), numio); }
+#define WRITE_BINARY_NAIVE(start, size, num, f)   {size_t numio = fwrite ((start), (size), (num), (f)); if (numio!=(num)) IOERR((f), (num), numio); }
 
 typedef std::vector<clp::DoublePoint> DPath;
 typedef std::vector<DPath>            DPaths;
-
-typedef std::vector<FILE*> FILES;
 
 typedef union T64 {
     clp::cInt i;
@@ -40,27 +40,52 @@ typedef union T64 {
     T64(double    _d) : d(_d) {}
 } T64;
 
-inline void   readInt64(FILE * f, int64 &v)  {  READ_BINARY(&v, sizeof(int64),  1, f); }
-inline void  writeInt64(FILE * f, int64 &v)  { WRITE_BINARY(&v, sizeof(int64),  1, f); }
-inline void  readDouble(FILE * f, double &v) {  READ_BINARY(&v, sizeof(double), 1, f); }
-inline void writeDouble(FILE * f, double &v) { WRITE_BINARY(&v, sizeof(double), 1, f); }
-inline void     readT64(FILE *f, T64 *vals, size_t numvals) {  READ_BINARY(vals, sizeof(T64), numvals, f); }
-inline void    writeT64(FILE *f, T64 *vals, size_t numvals) { WRITE_BINARY(vals, sizeof(T64), numvals, f); }
+class IOErr {
+public:
+    const char * message;
+    const char * function;
+    FILE * f;
+    int errn;
+    bool writing;
+    IOErr()                                             { clear(); }
+    IOErr        (FILE * _f, bool w, const char * func) { setError(_f, w, func); }
+    void clear()                                        { f = NULL; errn = 0;     message = NULL;           function = NULL;}
+    void setError(FILE * _f, bool w, const char * func) { f = _f;   errn = errno; message = strerror(errn); function = func; }
+};
+
+//keep track or errors
+class IOPaths {
+public:
+    std::vector<IOErr> errs;
+    FILE *f;
+    IOPaths()          : f(NULL) {}
+    IOPaths(FILE * _f) : f(_f) {}
+    inline void clear()    { errs.clear(); }
+    inline bool hasError() { return !errs.empty(); }
+    inline bool     readInt64(int64 &v)            { size_t numio = fread (&v, sizeof(v),      1, f); if (numio != 1) { errs.push_back(IOErr(f, false, "readInt64"   )); return false; }; return true; }
+    inline bool    writeInt64(int64 v)             { size_t numio = fwrite(&v, sizeof(v),      1, f); if (numio != 1) { errs.push_back(IOErr(f, true,  "writeInt64"  )); return false; }; return true; }
+    inline bool    readDouble(double &v)           { size_t numio = fread (&v, sizeof(v),      1, f); if (numio != 1) { errs.push_back(IOErr(f, false, "readDouble"  )); return false; }; return true; }
+    inline bool   writeDouble(double v)            { size_t numio = fwrite(&v, sizeof(v),      1, f); if (numio != 1) { errs.push_back(IOErr(f, true,  "writeDouble" )); return false; }; return true; }
+    inline bool      readT64P(T64 *v, size_t n)    { size_t numio = fread (v,  sizeof(T64),    n, f); if (numio != n) { errs.push_back(IOErr(f, false, "readT64"     )); return false; }; return true; }
+    inline bool     writeT64P(T64 *v, size_t n)    { size_t numio = fwrite(v,  sizeof(T64),    n, f); if (numio != n) { errs.push_back(IOErr(f, true,  "writeT64"    )); return false; }; return true; }
+    inline bool    readInt64P(int64  *v, size_t n) { size_t numio = fread (v,  sizeof(int64),  n, f); if (numio != n) { errs.push_back(IOErr(f, false, "readInt64*"  )); return false; }; return true; }
+    inline bool   writeInt64P(int64  *v, size_t n) { size_t numio = fwrite(v,  sizeof(int64),  n, f); if (numio != n) { errs.push_back(IOErr(f, true,  "writeInt64*" )); return false; }; return true; }
+    inline bool   readDoubleP(double *v, size_t n) { size_t numio =  fread(v,  sizeof(double), n, f); if (numio != n) { errs.push_back(IOErr(f, false, "readDouble*" )); return false; }; return true; }
+    inline bool  writeDoubleP(double *v, size_t n) { size_t numio = fwrite(v,  sizeof(double), n, f); if (numio != n) { errs.push_back(IOErr(f, true,  "writeDouble*")); return false; }; return true; }
+
+    bool   readClipperPaths(clp::Paths &paths);
+    bool  writeClipperPaths(clp::Paths &paths, PathCloseMode mode);
+
+    bool   readDoublePaths(clp::Paths  &paths, double scalingfactor);
+    bool   readDoublePaths(DPaths  &paths);
+    bool  writeDoublePaths(DPaths  &paths, PathCloseMode mode);
+    bool  writeDoublePaths(clp::Paths  &paths, double scalingfactor, PathCloseMode mode);
+
+    bool  writePrefixedClipperPaths(clp::Paths &paths, PathCloseMode mode);
+    bool   readPrefixedClipperPaths(clp::Paths &paths);
+};
 
 //this assumes that sizeof(clp::cInt)==sizeof(double)==sizeof(T64)==8
 int getPathsSerializedSize(clp::Paths &paths, PathCloseMode mode);
-
-void   readClipperPaths (FILE *f, clp::Paths &paths);
-void  writeClipperPaths (FILE *f, clp::Paths &paths, PathCloseMode mode);
-void  writeClipperSlice (FILE *f, clp::Paths &paths, std::vector<double> &zs, PathCloseMode mode);
-
-void   readDoublePaths  (FILE *f, clp::Paths  &paths, double scalingfactor);
-void   readDoublePaths  (FILE *f,     DPaths  &paths);
-void  writeDoublePaths  (FILE *f,     DPaths  &paths,                       PathCloseMode mode);
-void  writeDoublePaths  (FILE *f, clp::Paths  &paths, double scalingfactor, PathCloseMode mode);
-void  writeDoublePaths  (FILES fs,clp::Paths  &paths, double scalingfactor, PathCloseMode mode);
-
-void  writePrefixedClipperPaths(FILE *f, clp::Paths &paths, PathCloseMode mode);
-void   readPrefixedClipperPaths(FILE *f, clp::Paths &paths);
 
 #endif
